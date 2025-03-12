24059d2b6af527515f88d9c14240808310429f77
Omar Yova Aviles <yovalaviles812@gmail.com>
2025-03-11 19:09:24 -0600

    Uso de variables env

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-const cors = require('cors');
+const routes = require("./routes");
+const cors = require("cors");
 
 // Use CORS middleware
-app.use(cors({
-  origin: 'http://localhost:8081',  // Cambiado a la URL de tu frontend React
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:8081", // Si no está definida en el .env, usa la URL por defecto
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware para parsear el cuerpo de la solicitud en formato JSON
 app.use(express.json());
 
 // Rutas de la API
-app.use('/api', routes);
-
+app.use("/api", routes);
 
 // Puerto
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`API server esperando en el puerto ${PORT}`);
+  console.log(`API server esperando en el puerto ${PORT}`);
 });
